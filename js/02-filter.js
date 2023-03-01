11e787d69aa0d7e66e7308b21a202424e3d5b579
Bohdan Polishchuk <polishuk.bog@gmail.com>
2023-03-01 17:38:21 +0200

    use filter

--- js/02-filter.js
+++ js/02-filter.js
@@ -14,12 +14,6 @@ const tech = [
   { label: 'MongoDB' },
 ];
 
-/*
- * 1. Рендерим разметку элементов списка
- * 2. Слушаем изменение фильтра
- * 3. Фильтруем данные и рендерим новые элементы
- */
-
 const refs = {
   list: document.querySelector('.js-list'),
   input: document.querySelector('#filter'),
@@ -30,20 +24,17 @@ refs.input.addEventListener('input', _.debounce(onFilterChange, 300));
 const listItemsMarkup = createListItemsMarkup(tech);
 populateList(listItemsMarkup);
 
-function createListItemsMarkup(items) {
-  return items.map(item => `<li>${item.label}</li>`).join('');
-}
-
-function onFilterChange(evt) {
-  console.log('INPUT');
-  const filter = evt.target.value.toLowerCase();
-
-  const filteredItems = tech.filter(t =>
-    t.label.toLowerCase().includes(filter),
+function onFilterChange(e) {
+  const filter = e.target.value.toLowerCase();
+  const filteredItems = tech.filter(element =>
+    element.label.toLowerCase().includes(filter),
   );
-
   const listItemsMarkup = createListItemsMarkup(filteredItems);
   populateList(listItemsMarkup);
+}
+
+function createListItemsMarkup(items) {
+  return items.map(item => `<li>${item.label}</li>`).join('');
 }
 
 function populateList(markup) {
